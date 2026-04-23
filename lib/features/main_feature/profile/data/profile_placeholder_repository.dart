bdd67890895d8597d64a6fb7358bf32cc66927ad
VuittonVU <vuittonvarianu@gmail.com>
2026-04-23 08:57:46 +0700

    feat: add backend login dan profile

--- lib/features/main_feature/profile/data/profile_placeholder_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../models/profile_model.dart';
-
-class ProfilePlaceholderRepository {
-  Future<ProfileModel> getProfile() async {
-    await Future.delayed(const Duration(milliseconds: 300));
-
-    return ProfileModel.placeholder();
-  }
-}
